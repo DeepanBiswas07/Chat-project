d2ccbc7e1bb7905d970c94a209f13224a9051232
DeepanBiswas07 <deepanbiswas07@gmail.com>
2026-04-27 17:10:36 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ console.log("🔥 DB FILE LOADED");
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb://Deepan:hello1234@ac-hzfqkix-shard-00-00.qolxnte.mongodb.net:27017,ac-hzfqkix-shard-00-01.qolxnte.mongodb.net:27017,ac-hzfqkix-shard-00-02.qolxnte.mongodb.net:27017/?ssl=true&replicaSet=atlas-2otlib-shard-0&authSource=admin&appName=Cluster0"
+  "MONGODB ATLAS APIKEY"
 );
 
 const db = mongoose.connection;
